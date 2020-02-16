4e464b5eeed45c2d0cc960e09c10f574d024da08
zeke <eer2138@columbia.edu>
2020-02-15 19:40:08 -0500

    wip

--- src/reddit_client.rs
+++ src/reddit_client.rs
@@ -2,6 +2,8 @@ use std::fs;
 use serde::Deserialize;
 use std::collections::HashSet;
 
+const USER_AGENT: &str = "simpleRedditClient/0.1 by ZestyZeke";
+
 #[derive(Deserialize, Debug)]
 struct AuthResponse {
     access_token: String,
@@ -10,7 +12,6 @@ struct AuthResponse {
     scope: String
 }
 
-//TODO: put in separate class...
 pub struct RedditClient {
     username: String,
     password: String,
@@ -18,7 +19,41 @@ pub struct RedditClient {
     client_secret: String
 }
 
-const USER_AGENT: &str = "simpleRedditClient/0.1 by ZestyZeke";
+async fn get_saved_posts(endpoint: &str, 
+                         client: &reqwest::Client, 
+                         auth_info: AuthResponse,
+                         last_received: Option<String>) ->
+    Result<serde_json::Value, Box<dyn std::error::Error>> {
+
+    let api_name = "https://oauth.reddit.com";
+    let mut api_url = format!("{}{}", api_name, endpoint).to_string();
+    if let Some(last) = &last_received {
+        api_url = format!("{}?after={}", api_url, last).to_string();
+    }
+
+    let auth_value = format!("{} {}", auth_info.token_type, auth_info.access_token);
+    let builder = client.get(&api_url)
+        .header("Authorization", auth_value)
+        .header("User-Agent", USER_AGENT);
+    let resp = builder.send()
+        .await?
+        .json::<serde_json::Value>()
+        .await?;
+    //TODO: do something with the resp status?
+    //let resp_status = resp.status();
+
+    //TODO: check rate limiting...
+    //println!("{}", resp.text().await?);
+
+    Ok(resp["data"].clone())
+}
+
+fn parse_response(saved_posts: &serde_json::Value, filter: &str) -> String {
+
+
+    "".to_string()
+}
+
 
 impl RedditClient {
     pub fn new() -> RedditClient {
@@ -59,45 +94,17 @@ impl RedditClient {
         Ok(auth_response)
     }
 
-    async fn get_saved_posts(&self, endpoint: &str, 
-                             client: &reqwest::Client, 
-                             auth_info: AuthResponse,
-                             last_received: Option<String>) ->
-        Result<serde_json::Value, Box<dyn std::error::Error>> {
-
-        let api_name = "https://oauth.reddit.com";
-        let mut api_url = format!("{}{}", api_name, endpoint).to_string();
-        if let Some(last) = &last_received {
-            api_url = format!("{}?after={}", api_url, last).to_string();
-        }
-
-        let auth_value = format!("{} {}", auth_info.token_type, auth_info.access_token);
-        let builder = client.get(&api_url)
-            .header("Authorization", auth_value)
-            .header("User-Agent", USER_AGENT);
-        let resp = builder.send()
-            .await?
-            .json::<serde_json::Value>()
-            .await?;
-        //TODO: do something with the resp status?
-        //let resp_status = resp.status();
-
-        //TODO: check rate limiting...
-        //println!("{}", resp.text().await?);
-
-        Ok(resp["data"].clone())
-    }
-
     pub async fn run(&self, filter: &str, should_map: bool) -> 
         Result<(), Box<dyn std::error::Error>> {
         let client = reqwest::Client::new();
         let auth_response = self.authorize(&client).await?;
 
         let endpoint = format!("/user/{}/saved", self.username);
-        let saved_posts = self.get_saved_posts(&endpoint, &client, auth_response, None)
+        let saved_posts = get_saved_posts(&endpoint, &client, auth_response, None)
             .await?;
 
-        let mut seen_posts = HashSet::new();
+        //let mut seen_posts = HashSet::new();
+        parse_response(&saved_posts, filter);
 
         Ok(())
     }
